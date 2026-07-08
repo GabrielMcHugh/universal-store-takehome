8a95c4595d2f6d4e397e95ea277820bc61d89889
Gabriel McHugh <gabriel.mchugh@leap.com.au>
2026-07-08 15:27:31 +1000

    plp: added config file for apis

---
+++ services/product-landing-page/src/config/api.ts
@@ -0,0 +1,5 @@
+export const CATALOG_URL =
+  process.env.REACT_APP_CATALOG_URL ?? 'http://localhost:3000';
+
+export const INVENTORY_URL =
+  process.env.REACT_APP_INVENTORY_URL ?? 'http://localhost:4000';
