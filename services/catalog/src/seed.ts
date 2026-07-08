0983d5805eb3ce650a8ae08ae783b117775ab021
Gabriel McHugh <gabriel.mchugh@leap.com.au>
2026-07-08 12:16:54 +1000

    catalog: add seed data

--- services/catalog/src/seed.ts
+++ services/catalog/src/seed.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
+import { Catalog } from "./model/catalog";
 
 const mongoUrl = process.env.MONGO_URL as string;
 
 async function seedDatabase() {
   await mongoose.connect(mongoUrl);
+  await Catalog.deleteMany({});
+  await Catalog.create({ sku: '111111', title: "Pen", image: "S3/URL/1",  price : 3 });
+  await Catalog.create({ sku: '222222', title: "Paper", image: "S3/URL/2",  price : 1 });
+  await Catalog.create({ sku: '333333', title: "Key", image: "S3/URL/3",  price : 7 });
+  await Catalog.create({ sku: '444444', title: "Marker", image: "S3/URL/4",  price : 2 });
   await mongoose.connection.close();
 }
 
