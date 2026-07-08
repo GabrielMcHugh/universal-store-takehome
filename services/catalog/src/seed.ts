490260370daa211e32036694ed49b76c8f1e202e
Gabriel McHugh <gabriel.mchugh@leap.com.au>
2026-07-08 18:31:20 +1000

    catalog: updated the image seed with test urls

--- services/catalog/src/seed.ts
+++ services/catalog/src/seed.ts
@@ -6,10 +6,10 @@ const mongoUrl = process.env.MONGO_URL as string;
 async function seedDatabase() {
   await mongoose.connect(mongoUrl);
   await Catalog.deleteMany({});
-  await Catalog.create({ sku: '111111', title: "Pen", image: "S3/URL/1",  price : 3 });
-  await Catalog.create({ sku: '222222', title: "Paper", image: "S3/URL/2",  price : 1 });
-  await Catalog.create({ sku: '333333', title: "Key", image: "S3/URL/3",  price : 7 });
-  await Catalog.create({ sku: '444444', title: "Marker", image: "S3/URL/4",  price : 2 });
+  await Catalog.create({ sku: '111111', title: "Pen", image: "https://picsum.photos/200",  price : 3 });
+  await Catalog.create({ sku: '222222', title: "Paper", image: "https://picsum.photos/200",  price : 1 });
+  await Catalog.create({ sku: '333333', title: "Key", image: "https://picsum.photos/200",  price : 7 });
+  await Catalog.create({ sku: '444444', title: "Marker", image: "https://picsum.photos/200",  price : 2 });
   await mongoose.connection.close();
 }
 
