00d6aa0b38ef7692996902efadf5be1403d41662
Gabriel McHugh <gabriel.mchugh@leap.com.au>
2026-07-08 18:24:58 +1000

    plp: added catalog to app and added some css classes

--- services/product-landing-page/src/App.tsx
+++ services/product-landing-page/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { ProductCatalog } from './components/ProductCatalog';
 import { useProducts } from './hooks/useProduct';
 
 function App() {
@@ -10,6 +11,10 @@ function App() {
       <header className="App-header">
         <img src="https://fulfil.universalstore.com/static/version1717065130/frontend/universalstore/universalstore/default/images/logo.svg" className="App-logo" alt="logo" />
       </header>
+
+      <main className='App=main'>
+        <ProductCatalog/>
+      </main>
     </div>
   );
 }
