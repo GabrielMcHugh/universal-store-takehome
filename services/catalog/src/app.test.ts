b8f8a1d5994538fff1cc81c63c623f33ee327a57
Gabriel McHugh <gabriel.mchugh@leap.com.au>
2026-07-08 13:31:25 +1000

    catalog: added fixture data factory to test file

--- services/catalog/src/app.test.ts
+++ services/catalog/src/app.test.ts
@@ -3,8 +3,21 @@ import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { createApp } from './app';
 import { Catalog } from './model/catalog';
+import { CatalogItem } from './types/catalogItem';
 
-//Happy Path
+//Data factory
+export const catalogItemFixture = (
+    overrides: Partial<CatalogItem> = {}
+): CatalogItem => ({
+    sku: "SKU-001",
+    title: "Test Product",
+    price: 29.99,
+    image: "https://example.com/image.jpg",
+})
+
+const singleItemTest: CatalogItem = catalogItemFixture()
+ 
+
 describe("GET /catalog", () => {
     let mongo: MongoMemoryServer;
     const app = createApp();
@@ -23,17 +36,25 @@ describe("GET /catalog", () => {
         await Catalog.deleteMany({})
     })
 
+
+    //Happy Path
     it("returns catalog items", async () => {
-        await Catalog.create({
-            sku: "SKU-001",
-            title: "Test Product",
-            price: 29.99,
-            image: "https://example.com/image.jpg",
-        });
+        await Catalog.create(singleItemTest);
 
         const res = await request(app).get("/catalog");
 
         expect(res.status).toBe(200);
         expect(res.body).toHaveLength(1);
+    });
+
+
+    //Shape
+    it("returns ")
+
+    //Empty Db
+    it('returns an empty array when no items exist', async () => {
+        const res = await request(app).get('/catalog');
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual([]);
     });
 });
